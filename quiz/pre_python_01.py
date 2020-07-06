269ffb132214c2be93ac7eb85a9c7dcc1301fb3d
lumos1211 <67613714+lumos1211@users.noreply.github.com>
2020-07-06 19:48:18 +0900

    commit

--- quiz/pre_python_01.py
+++ quiz/pre_python_01.py
@@ -13,3 +13,11 @@
 ★★★★★
 ★★★★★
  """
+
+
+num = [int(input("가로의 숫자를 입력하시오 : ")), int(input("세로의 숫자를 입력하시오 : "))]
+
+for i in range(num[1]):
+    print(" ★ "*num[0])
+
+
--- quiz/pre_python_02.py
+++ quiz/pre_python_02.py
@@ -10,3 +10,25 @@
 <출력>
 150
 """
+
+
+num = [int(input("첫 번째 수를 입력하세요 : ")), int(input("두 번째 수를 입력하세요 : ")), input("어떤 연산을 하실 건가요? : ")]
+cal = ['+','-','*','/','%','//']
+
+if num[2] in cal :
+    if num[2] == cal[0] :
+        print("%d + %d = %d" %(num[0],num[1],num[0]+num[1]))
+    elif num[2] == cal[1] :
+        print("%d - %d = %d" %(num[0],num[1],num[0]-num[1]))
+    elif num[2] == cal[2] :
+        print("%d * %d = %d" %(num[0],num[1],num[0]*num[1]))
+    elif num[2] == cal[3] :
+        print("%d / %d = %.4f" %(num[0],num[1],num[0]/num[1]))
+    elif num[2] == cal[4] :
+        print("%d % %d = %d" %(num[0],num[1],num[0]%num[1]))
+    elif num[2] == cal[5] :
+        print("%d // %d = %d" %(num[0],num[1],num[0]//num[1]))
+else :
+    print("error")
+
+
